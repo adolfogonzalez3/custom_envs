Improved performance by fixing errors. The first error was in the gradient formula which was using mse. Should be using cross entropy. The second error was in the softmax function. Was doing an improper method of avoiding NaN's.

custom_envs/optimize.py
@@ -8,51 +8,16 @@ import cv2 as cv
 from gym import core, spaces
 from gym.utils import seeding
 
-import mnist
-
-from stable_baselines.ppo2 import PPO2
-from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
-from stable_baselines.common.vec_env import DummyVecEnv
 __author__ = "Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>"
 
 from custom_envs.load_data import load_data
 from custom_envs.logger import Logger
+from custom_envs.utils import shuffle, to_onehot, softmax, cross_entropy
+from custom_envs.model import Model
 
 LOG_EVERY = 1000
 # SOURCE:
 # https://github.com/rlpy/rlpy/blob/master/rlpy/Domains/Acrobot.py
-
-def shuffle(*args, np_random=npr):
-    length = len(args[0])
-    indices = np.arange(length)
-    npr.shuffle(indices)
-    return [arg[indices] for arg in args]
-
-def cross_entropy(p, y):
-    #p_log = np.nan_to_num(np.log(p))
-    p_log = np.log(p+1e-16)
-    return np.mean(np.sum(-p_log*y, axis=1))
-    
-def mse(p, y):
-    return np.mean(np.sum((p - y)**2, axis=1)/2)
-
-def softmax(x):
-    x = (x - np.max(x))/(np.max(x) - np.min(x) + 1e-8)
-    #p_exp = np.exp(x - np.max(x))
-    p_exp = np.exp(x)
-    p_sum = np.sum(p_exp, axis=1)
-    return p_exp/p_sum[:, None]
-
-def sigmoid(x):
-    return numexpr.evaluate('1 / (1 + exp(-x - 1e-8))')
-
-def to_onehot(x):
-    x = x.astype(np.int).ravel()
-    num_of_labels = np.unique(x).size
-    onehot = np.zeros((len(x), num_of_labels))
-    onehot[np.arange(len(x)), x] = 1
-    return onehot, num_of_labels
 
 class Optimize(core.Env):
 
@@ -87,8 +52,10 @@ class Optimize(core.Env):
         #samples = [cv.resize(s, (14, 14)).ravel() for s in samples]
         #samples = np.stack(samples)
         self.samples = samples
+        self.features = samples
         self.labels, num_of_labels = to_onehot(labels)
         self.feature_size = samples.shape[-1]
+        self.model = Model(self.feature_size, num_of_labels)
         self.H = 3
         self.obj_list = np.zeros(self.H)
         self.grad_list = np.zeros((self.H, self.feature_size, num_of_labels))
@@ -119,12 +86,14 @@ class Optimize(core.Env):
     def reset(self):
         self.logger = Logger('OptDist-v0:iris', self.seed_no)
         if self.rewards:
-            sw = self.samples @ self.w
+            #sw = self.samples @ self.w
             #prediction = sigmoid(sw)
-            prediction = softmax(sw)
-            prediction = np.argmax(prediction, axis=1)
-            labels = np.argmax(self.labels, axis=1)
-            self.running_acc.append(np.mean(prediction == labels))
+            #prediction = softmax(sw)
+            #prediction = np.argmax(prediction, axis=1)
+            #labels = np.argmax(self.labels, axis=1)
+            #accuracy = np.mean(prediction == labels)
+            accuracy = self.model.compute_accuracy(self.features, self.labels)
+            self.running_acc.append(accuracy)
             self.running_rewards.append(np.sum(self.rewards))
         if len(self.running_rewards) >= LOG_EVERY:
             avg_rew = np.mean(self.running_rewards)
@@ -145,6 +114,7 @@ class Optimize(core.Env):
         #self.w.fill(0)
         self.W_list.fill(0)
         I = 0
+        self.model.reset(self.np_random)
         return np.concatenate([self.W_list[I].ravel(), self.obj_list[[I]],
                                 self.grad_list[I].ravel()])
 
@@ -152,18 +122,23 @@ class Optimize(core.Env):
         self.global_steps += 1
         #batch_size = 128
         I = self.steps % self.H
-        old_w = self.w.copy()
-        self.w = self.w - a.reshape((-1, self.num_of_labels))
+        #old_w = self.w.copy()
+        #self.w = self.w - a.reshape((-1, self.num_of_labels))
         labels = self.labels#[batch_size*I:batch_size*(I+1)]
         samples = self.samples#[batch_size*I:batch_size*(I+1)]
-        sw = samples @ self.w
+        features = samples
+        #sw = samples @ self.w
         #prediction = sigmoid(sw)
-        prediction = softmax(sw)
+        #prediction = softmax(sw)
         #objective = mse(prediction, labels)
-        objective = cross_entropy(prediction, labels)
-        gradient = numexpr.evaluate('(-prediction*(1-prediction)) * (labels - prediction)')
-        gradient = (samples.T @ gradient)
+        #objective = cross_entropy(prediction, labels)
+        #gradient = numexpr.evaluate('(-prediction*(1-prediction)) * (labels - prediction)')
+        #gradient = (samples.T @ gradient)
         
+        
+        self.model.set_weights(self.model.weights + a.reshape((-1, self.num_of_labels)))
+        objective, gradient, _ = self.model.compute_backprop(features, labels)
+
         self.obj_list[I] = (objective - self.obj_list[I-1]) / (self.obj_list[I-1] + 0.1)
         self.grad_list[I] = gradient / (np.abs(self.grad_list[I-1])+1)
         self.W_list[I] = np.abs(self.W_list[I-1] - self.W_list[I-2])/(np.abs(self.W_list[I] - self.W_list[I-1]) + 0.1)
@@ -172,22 +147,18 @@ class Optimize(core.Env):
         state = np.concatenate([self.W_list[I].ravel(), self.obj_list[[I]],
                                 self.grad_list[I].ravel()])
         terminal = self._terminal()
-        sw = self.samples @ self.w
-        prediction = softmax(sw)
+        #sw = self.samples @ self.w
+        #prediction = softmax(sw)
         #print(prediction[:20])
         #prediction = sigmoid(sw)
         #objective = mse(prediction, self.labels)
-        objective = cross_entropy(prediction, self.labels)
+        #objective = cross_entropy(prediction, self.labels)
         reward = -objective
         #print(reward)
         self.rewards.append(reward)
         accuracy = None
         if terminal:
-            sw = self.samples @ self.w
-            prediction = softmax(sw)
-            prediction = np.argmax(prediction, axis=1)
-            labels = np.argmax(self.labels, axis=1)
-            accuracy = np.mean(prediction == labels)
+            accuracy = self.model.compute_accuracy(features, labels)
         return state, reward, terminal, {'objective': objective, 'accuracy': accuracy}
 
     def _terminal(self):
@@ -201,6 +172,10 @@ class Optimize(core.Env):
 
 
 if __name__ == '__main__':
+    from stable_baselines.ppo2 import PPO2
+    from stable_baselines.common.policies import MlpPolicy
+    from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+    from stable_baselines.common.vec_env import DummyVecEnv
     env = DummyVecEnv([Optimize])
     agent = PPO2(MlpPolicy, env)
     agent.learn(total_timesteps=2**20)