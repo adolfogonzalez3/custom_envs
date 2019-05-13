b587df34a0ed77bc1d3d3ebb4559ce49cd23e859
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-13 14:31:27 -0500

    Added global variable for testing control

--- tests/test_mailboxinsync.py
+++ tests/test_mailboxinsync.py
@@ -12,6 +12,7 @@ from custom_envs.multiagent.MailboxInSync import MailboxInSync
 
 EXECUTORS = (ThreadPoolExecutor, ProcessPoolExecutor)
 JOB_EXE = [Process, Thread]
+NUMBER_OF_PROCESSORS = 2
 
 def task(L):
     with L:
@@ -21,32 +22,32 @@ def task(L):
 def task_sudden_stop(L):
     with L:
         ID = L.get()
-        if ID != 4:
+        if ID != NUMBER_OF_PROCESSORS - 1:
             L.append(ID)
 
 def task_sudden_error(L):
     with L:
         ID = L.get()
-        if ID != 4:
+        if ID != NUMBER_OF_PROCESSORS - 1:
             raise RuntimeError
 
 @pytest.mark.parametrize("pool_executor", EXECUTORS)
 def test_mailboxinsync_data_correctness_0(pool_executor):
     mailbox = MailboxInSync()
-    with pool_executor(5) as executor:
-        executor.map(task, [mailbox.spawn() for _ in range(5)])
-        mailbox.append([i for i in range(5)])
+    with pool_executor(2) as executor:
+        executor.map(task, [mailbox.spawn() for _ in range(2)])
+        mailbox.append([i for i in range(NUMBER_OF_PROCESSORS)])
 
         data = sorted(mailbox.get())
-        assert(sorted(data) == [i for i in range(5)])
+        assert(sorted(data) == [i for i in range(2)])
 
 
 @pytest.mark.parametrize("pool_executor", EXECUTORS)
 def test_mailboxinsync_sudden_closing(pool_executor):
     mailbox = MailboxInSync()
-    with pool_executor(5) as executor:
-        executor.map(task_sudden_stop, [mailbox.spawn() for _ in range(5)])
-        mailbox.append([i for i in range(5)])
+    with pool_executor(2) as executor:
+        executor.map(task_sudden_stop, [mailbox.spawn() for _ in range(2)])
+        mailbox.append([i for i in range(NUMBER_OF_PROCESSORS)])
 
         if not mailbox.is_broken() and mailbox.poll():
             assert True
@@ -54,9 +55,9 @@ def test_mailboxinsync_sudden_closing(pool_executor):
 @pytest.mark.parametrize("pool_executor", EXECUTORS)
 def test_mailboxinsync_sudden_error(pool_executor):
     mailbox = MailboxInSync()
-    with pool_executor(5) as executor:
-        executor.map(task_sudden_error, [mailbox.spawn() for _ in range(5)])
-        mailbox.append([i for i in range(5)])
+    with pool_executor(2) as executor:
+        executor.map(task_sudden_error, [mailbox.spawn() for _ in range(2)])
+        mailbox.append([i for i in range(NUMBER_OF_PROCESSORS)])
 
         if not mailbox.is_broken() and mailbox.poll():
             assert True
