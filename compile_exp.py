a6bc4d53000ad894b286f424c90c07e6669fdafa
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-13 12:06:36 -0500

    Use chunksize and moved script code

--- compile_exp.py
+++ compile_exp.py
@@ -35,8 +35,8 @@ def load_exprs(path):
     path = Path(path)
     expr_paths = [ename for ename in path.iterdir() if ename.is_dir()]
     with ProcessPoolExecutor() as executor:
-        expr_dfs = [t for t in tqdm(executor.map(load_expr, expr_paths),
-                                    total=len(expr_paths))]
+        expr_dfs = list(tqdm(executor.map(load_expr, expr_paths, chunksize=32),
+                             total=len(expr_paths)))
         #expr_dfs = executor.map(load_expr, expr_paths)
     return pd.concat(expr_dfs), expr_paths
 
@@ -51,19 +51,22 @@ def movingAverage(values, window):
     weights = np.repeat(1.0, window) / window
     return np.convolve(values, weights, 'valid')
 
-
-if __name__ == '__main__':
+def main():
     import argparse
-    PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("path", help="The path to the monitor files.")
-    PARSER.add_argument("name", help="Name of the compiled pickle file.")
-    ARGS = PARSER.parse_args()
-    PATH = Path(ARGS.path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help="The path to the monitor files.")
+    parser.add_argument("name", help="Name of the compiled pickle file.")
+    args = parser.parse_args()
+    path = Path(args.path)
     #expr_dict = get_experiments(PATH)
     #print(expr_dict)
-    if PATH.is_dir():
+    if path.is_dir():
         print('Loading...')
-        expr_df, experiment_names = load_exprs(PATH)
+        expr_df, experiment_names = load_exprs(path)
         expr_df.reset_index(inplace=True)
         print('Saving...')
-        expr_df.to_pickle('{}.pkl'.format(ARGS.name))
+        expr_df.to_pickle('{}.pkl'.format(args.name))
+
+
+if __name__ == '__main__':
+    main()
