3536b010ceee52d9056b5cd636f3ef3245fbd54d
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-13 12:08:41 -0500

    Added ability to load different data sets

--- custom_envs/data/load_data.py
+++ custom_envs/data/load_data.py
@@ -3,13 +3,17 @@ from pathlib import Path
 
 import numpy as np
 
+from custom_envs.utils import normalize
 
-def load_data(data_name='iris'):
-    name = None
+def load_data(name='iris'):
     path = Path(__file__).resolve().parent
-    if data_name == 'iris':
-        return np.load(path / 'iris.npz')['data']
-    elif data_name == 'mnist':
-        return np.load(path / 'mnist.npz')['data']
+    if name == 'iris' or name == 'mnist':
+        data = np.load((path / name).with_suffix('.npz'))['data']
+        return data[..., :-1], data[..., -1]
+    elif name == 'skin':
+        data = np.loadtxt(path / 'skin.txt', delimiter='\t')
+        features = np.zeros((data.shape[0], 4))
+        features[:, :3] = data[..., :-1]
+        return normalize(features), data[..., -1]
     else:
-        raise RuntimeError('No such data set named: {}'.format(data_name))
+        raise RuntimeError('No such data set named: {}'.format(name))
