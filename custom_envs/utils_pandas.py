5d239359c528fec18f741c3937ab4c8e657b4601
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-13 14:30:42 -0500

    Moved functions to separate file

---
+++ custom_envs/utils_pandas.py
@@ -0,0 +1,46 @@
+
+from itertools import product
+
+import numpy as np
+
+
+def get_unique(dataframe, *args):
+    """
+    Return the unique elements in each column specified.
+
+    :param df: (Pandas DataFrame) The dataframe which contains columns
+                                    specified in *args.
+    :param *args: ([str]) A list of strings which are a subset of the
+                            columns in df.
+    :return: ([Pandas Series]) A list of series which hold the unique
+                                elements in each column specified.
+    """
+    return [sorted(dataframe[col].unique()) for col in args]
+
+
+def iterate_levels(dataframe, levels=3):
+    """
+    Yield the levels in the dataframe as well as their associated rows.
+
+    :param dataframe: (pandas.DataFrame) A dataframe contained a tiered index
+                                         containing at least (levels) levels.
+    :param levels: (int) The number of levels to index by.
+    """
+    for idx in product(*dataframe.index.levels[:levels]):
+        yield idx, dataframe.loc[idx:idx]
+
+
+def create_grid(dataframe, levels=3):
+    """
+    Create grids and groups of rows based on a tiered index.
+
+    :param dataframe: (pandas.DataFrame) A dataframe contained a tiered index
+                                         containing at least (levels) levels.
+    :param levels: (int) The number of levels to index by.
+    """
+    idxs_groups = list(iterate_levels(dataframe, levels))
+    idxs, groups = zip(*idxs_groups)
+    idxs = zip(*idxs)
+    grid_shape = [len(l) for l in dataframe.index.levels[:levels]]
+    grids = [np.array(idx).reshape(grid_shape) for idx in idxs]
+    return grids, groups
--- eval_csv.py
+++ eval_csv.py
@@ -1,5 +1,7 @@
 
 from itertools import product
+from collections import defaultdict
+
 
 import pandas as pd
 import numpy as np
@@ -9,141 +11,74 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-def get_unique(dataframe, *args):
-    """
-    Returns the unique elements in each column specified.
+from custom_envs.utils_pandas import get_unique, create_grid
 
-    :param df: (Pandas DataFrame) The dataframe which contains columns
-                                    specified in *args.
-    :param *args: ([str]) A list of strings which are a subset of the
-                            columns in df.
-    :return: ([Pandas Series]) A list of series which hold the unique
-                                elements in each column specified.
-    """
-    return [sorted(dataframe[col].unique()) for col in args]
 
-def iterate_levels(dataframe, levels=3):
-    for idx in product(*dataframe.index.levels[:levels]):
-        print(idx)
-        yield idx, dataframe.loc[idx]
+def plot_two(dataframes, names):
+    axes = defaultdict(lambda: plt.figure().add_subplot(111))
+    for dataframe, name in zip(dataframes, names):
+        dataframe['learning_rate'] = np.log10(dataframe['learning_rate'])
+        groups = dataframe.groupby(['learning_rate', 'gamma', 'alg', 'index'])
+        mean_df = groups.mean()
+        std_df = groups.std()
+        (lrs, gammas, algs), groups = create_grid(mean_df)
+        metric = np.reshape([g['objective'].mean() for g in groups], lrs.shape)
 
-def create_gridv2(dataframe, levels=3):
-    idxs_groups = list(iterate_levels(dataframe, levels))
-    idxs, groups = zip(*idxs_groups)
-    idxs = zip(*idxs)
-    grid_shape = [len(l) for l in dataframe.index.levels[:levels]]
-    grids = [np.array(idx).reshape(grid_shape) for idx in idxs]
-    return grids, groups
-
-def create_grid(df):
-    learning_rates = sorted(df['learning_rate'].unique())
-    gammas = sorted(df['gamma'].unique())
-    algs = sorted(df['alg'].unique())
-    X = []
-    Y = []
-    Z = []
-    result_df = df.groupby(['learning_rate', 'gamma', 'alg', 'index']).mean()
-    for alg, lr, g in product(algs, learning_rates, gammas):
-        result = result_df.loc[(lr, g, alg, pd.IndexSlice['index', :])]
-        metric = result['objective']
-        X.append(lr)
-        Y.append(g)
-        Z.append(metric.mean())
-    grid_shape = (len(algs), len(learning_rates), len(gammas))
-    X = np.array(X, dtype=np.float).reshape(grid_shape)
-    Y = np.array(Y, dtype=np.float).reshape(grid_shape)
-    Z = np.array(Z, dtype=np.float).reshape(grid_shape)
-    return X, Y, Z, result_df
-
-def plot_two(df, df2):
-    df['learning_rate'] = np.log10(df['learning_rate'])
-    learning_rates = sorted(df['learning_rate'].unique())
-    gammas = sorted(df['gamma'].unique())
-    algs = sorted(df['alg'].unique())
-    result_df = df.groupby(['learning_rate', 'gamma', 'alg', 'index']).mean()
-    grids, groups = create_gridv2(result_df)
-    Z = np.array([g['objective'].mean()
-                  for g in groups]).reshape(grids[0].shape)
-    print(Z)
-    exit()
-    X, Y, Z, result_df = create_grid(df)
-    for i in product(*result_df.index.levels[0:3]):
-        print(result_df.loc[i])
-        print(i)
-        exit()
-    exit()
-    df2['learning_rate'] = np.log10(df['learning_rate'])
-    X2, Y2, Z2, result_df2 = create_grid(df2)
-
-    for i, name in enumerate(algs):
-        alg = None
-        if i == 0:
-            alg = 'PPO'
-        elif i == 1:
-            alg = 'A2C'
-        elif i == 2:
-            alg = 'DDPG'
-        pos = np.nanargmin(Z[i, ...])
-
-        lr = X[i,...].ravel()[pos]
-        g = Y[i,...].ravel()[pos]
-        result = result_df.loc[(lr, g, alg, pd.IndexSlice['index', :])]
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(range(len(result['r'])), result['objective'])
-        print(result)
-        print(Z[i])
-
-        lr = X2[i,...].ravel()[pos]
-        g = Y2[i,...].ravel()[pos]
-        result = result_df2.loc[(lr, g, alg, pd.IndexSlice['index', :])]
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.plot(range(len(result['r'])), result['objective'])
-        print(result)
-        print(Z[i])
+        algs = [a for a in algs[0, 0, :]]
+        for i, alg in enumerate(algs):
+            pos = np.nanargmin(metric[..., i])
+            lr = lrs[..., i].ravel()[pos]
+            g = gammas[..., i].ravel()[pos]
+            metric_mean = mean_df.loc[(lr, g, alg), 'objective']
+            metric_std = std_df.loc[(lr, g, alg), 'objective']
+            print('ALG: ', alg, ' Learning Rate: ', lr, ' Gamma: ', g)
+            axes[alg].plot(range(len(metric_mean)), metric_mean, label=name)
+            axes[alg].fill_between(range(len(metric_std)),
+                                   metric_mean + metric_std,
+                                   metric_mean - metric_std, alpha=0.1)
+    for (alg, axis) in axes.items():
+        axis.set_title(alg)
+        axis.set_xlabel('episodes')
+        axis.set_ylabel('objective')
+        axis.legend()
+    plt.show()
     
 
-def plot_hyperparamsearch_alg(df):
+def plot_hyperparamsearch_alg(dataframe):
     '''Plot experiments contained in dataframe.'''
 
-    df['learning_rate'] = np.log10(df['learning_rate'])
-    learning_rates = sorted(df['learning_rate'].unique())
-    gammas = sorted(df['gamma'].unique())
-    algs = sorted(df['alg'].unique())
-    X, Y, Z, _ = create_grid(df)
+    dataframe['learning_rate'] = np.log10(dataframe['learning_rate'])
+    groups = dataframe.groupby(['learning_rate', 'gamma', 'alg', 'index'])
+    mean_df = groups.mean()
+    std_df = groups.std()
+    (lrs, gammas, algs), groups = create_grid(mean_df)
+    Z = np.array([g['objective'].mean() for g in groups]).reshape(lrs.shape)
     metric_min = np.nanmin(Z)
     metric_max = np.nanmax(Z)
 
-    for i, name in enumerate(algs):
-        alg = None
-        if i == 0:
-            alg = 'PPO'
-        elif i == 1:
-            alg = 'A2C'
-        elif i == 2:
-            alg = 'DDPG'
+    for i, alg in enumerate(algs[0, 0, :]):
+        print(alg)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        #ax.scatter(learning_rate_list, gamma_list, steps)
-        ax.plot_surface(X[i], Y[i], Z[i], cmap=None, linewidth=0,
-                        antialiased=False)
+        ax.plot_surface(lrs[..., i], gammas[..., i], Z[..., i], cmap=None,
+                        linewidth=0, antialiased=False)
         ax.set_xlabel('Learning Rate')
         ax.set_ylabel('Gamma')
         ax.set_zlabel('Mean Loss')
         ax.set_zlim(metric_min, metric_max)
         ax.set_title('Model: {}'.format(alg))
-        #plt.show()
-        #pos = np.nanargmin(Z[i,...])
-        #lr = X[i,...].ravel()[pos]
-        #g = Y[i,...].ravel()[pos]
+        pos = np.nanargmin(Z[..., i])
+        lr = lrs[..., i].ravel()[pos]
+        g = gammas[..., i].ravel()[pos]
         #print(lr, g, alg, pos, X[i,...].size)
-        #result = result_df.loc[(lr, g, alg, pd.IndexSlice['index', :])]
-        #fig = plt.figure()
-        #ax = fig.add_subplot(111)
-        #print(result)
-        #print(Z[i])
-        #ax.plot(range(len(result['r'])), result['objective'])
+        metric_mean = mean_df.loc[(lr, g, alg), 'objective']
+        metric_std = std_df.loc[(lr, g, alg), 'objective']
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(range(len(metric_mean)), metric_mean)
+        ax.fill_between(range(len(metric_std)),
+                        metric_mean + metric_std,
+                        metric_mean - metric_std, alpha=0.1)
     plt.show()
     
 
@@ -208,21 +143,25 @@ def plot_best(df, df_lr):
 
 def main():
     import argparse
-    PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("file_name", help="The name of the file to load.")
-    ARGS = PARSER.parse_args()
-    FILENAME = ARGS.file_name
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", help="The name of the file to load.")
+    args = parser.parse_args()
 
     print('Reading...')
-    df = pd.read_pickle(FILENAME)
+    dataframe = pd.read_pickle(args.file_name)
     print('Plotting...')
-    plot_hyperparamsearch_alg(df)
+    plot_hyperparamsearch_alg(dataframe)
 
-if __name__ == '__main__':
+def main2():
     print('Reading...')
-    df = pd.read_pickle('multiagent.pkl')
-    #df2 = pd.read_pickle('single_agent.pkl')
+    #dataframe = pd.read_pickle('multiagent.pkl')
+    dataframe = pd.read_pickle('single_agent.pkl')
+    dataframe2 = pd.read_pickle('exploration_a2c.pkl')
     print('Plotting...')
     #df_lr = pd.read_csv('results_lr.csv', index_col=0)
     #plot_hyperparamsearch_LR()
-    plot_two(df, None)
+    plot_two([dataframe, dataframe2], ['single_agent',
+                                       'single_agent_with_lower_exploration'])
+
+if __name__ == '__main__':
+    main()
