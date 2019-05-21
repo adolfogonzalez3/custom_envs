a7baf1d56b5f5bb6c54e3e6ad52a0458e82e89ea
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-21 14:33:33 -0500

    Fixed import

--- custom_envs/data/load_data.py
+++ custom_envs/data/load_data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import numpy as np
 
-from custom_envs.utils import normalize
+from custom_envs.utils.utils_common import normalize, resize_all
 
 def load_data(name='iris'):
     '''
@@ -11,13 +11,16 @@ def load_data(name='iris'):
 
     '''
     path = Path(__file__).resolve().parent
-    if name == 'iris' or name == 'mnist':
+    if name == 'iris':
         data = np.load((path / name).with_suffix('.npz'))['data']
         return data[..., :-1], data[..., -1]
+    elif name == 'mnist':
+        data = np.load((path / name).with_suffix('.npz'))['data']
+        return normalize(resize_all(data[..., :-1])), data[..., -1]
     elif name == 'skin':
         data = np.loadtxt(path / 'skin.txt', delimiter='\t')
         features = np.zeros((data.shape[0], 4))
-        features[:, :3] = data[..., :-1]
-        return normalize(features), data[..., -1]
+        features[:, :3] = normalize(data[..., :-1])
+        return features, data[..., -1]
     else:
         raise RuntimeError('No such data set named: {}'.format(name))
