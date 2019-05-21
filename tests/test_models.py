6fce2ce9eb24cd187124ead4f51e54d23b8c672b
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-21 14:21:56 -0500

    Merged tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,41 +3,54 @@ import pytest
 import numpy as np
 import numpy.random as npr
 
-from custom_envs.models.model_tf import ModelTF as Model
+from custom_envs.models import ModelTF, ModelKeras, ModelNumpy
+
+MODELS = [ModelNumpy, ModelKeras, ModelTF]
 
 
-def test_size():
+@pytest.mark.parametrize("Model", MODELS)
+def test_size(Model):
     model = Model(4, 3)
     assert model.size == 12
 
-def test_forward():
+
+@pytest.mark.parametrize("Model", MODELS)
+def test_forward(Model):
     model = Model(4, 3)
     features = np.ones((32, 4))
     a = model.forward(features)
     assert a.shape == (32, 3)
 
-def test_compute_loss():
+
+@pytest.mark.parametrize("Model", MODELS)
+def test_compute_loss(Model):
     model = Model(4, 3)
     features = np.ones((32, 4))
     labels = npr.randint(3, size=(32, 3))
     loss = model.compute_loss(features, labels)
     assert isinstance(loss, float)
 
-def test_compute_gradients():
+
+@pytest.mark.parametrize("Model", MODELS)
+def test_compute_gradients(Model):
     model = Model(4, 3)
     features = np.ones((32, 4))
     labels = npr.randint(3, size=(32, 3))
     grad = model.compute_gradients(features, labels)
     assert grad.shape == (4, 3)
 
-def test_compute_accuracy():
+
+@pytest.mark.parametrize("Model", MODELS)
+def test_compute_accuracy(Model):
     model = Model(4, 3)
     features = np.ones((32, 4))
     labels = npr.randint(3, size=(32, 3))
     acc = model.compute_loss(features, labels)
     assert isinstance(acc, float)
 
-def test_compute_backprop():
+
+@pytest.mark.parametrize("Model", MODELS)
+def test_compute_backprop(Model):
     model = Model(4, 3)
     features = np.ones((32, 4))
     labels = npr.randint(3, size=(32, 3))
