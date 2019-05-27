e53001156ef3064648f7909009707d368ddd52d5
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-26 21:25:19 -0500

    Created base class for environments

---
+++ custom_envs/envs/baseenvironment.py
@@ -0,0 +1,58 @@
+'''A module that contains an abstract environment class.'''
+
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+
+import numpy.random as npr
+from gym import Env
+from gym.utils.seeding import np_random
+
+from custom_envs.utils.utils_common import use_random_state
+
+
+class BaseEnvironment(Env):
+    '''
+    An abstract class inherited by all environments in the package.
+    '''
+
+    def __init__(self):
+        self.random_generator, _ = np_random()
+        self.current_step = 0
+
+    def seed(self, seed=None):
+        '''
+        Seed the environment's random generator.
+
+        :param seed: (int or None) If an integer then seed is used to seed the
+                                   random generator otherwise if None then
+                                   use seed = 0.
+        '''
+        self.random_generator, _ = np_random(seed)
+
+    def step(self, action):
+        '''
+        Take one step in the environment.
+
+        :param action: (numpy.array) An action that follows the rules of the
+                                     action space.
+        '''
+        with use_random_state(self.random_generator):
+            state, reward, terminal, info = self._step(action)
+        info['episode'] = {'r': reward, 'l': self.current_step}
+        return state, reward, terminal, info
+
+    def reset(self):
+        '''
+        Reset the environment.
+        '''
+        self.current_step = 0
+        with use_random_state(self.random_generator):
+            return self._reset()
+
+    @abstractmethod
+    def _step(self, action):
+        pass
+
+    @abstractmethod
+    def _reset(self):
+        pass
--- custom_envs/envs/optimize.py
+++ custom_envs/envs/optimize.py
@@ -1,32 +1,28 @@
-"""classic Acrobot task"""
+"""The Optimize environment."""
 
 import numpy as np
 import numpy.random as npr
 
-from gym import core, spaces
-from gym.utils import seeding
+from gym.spaces import Box
+
+from custom_envs import load_data
+from custom_envs.models import ModelNumpy as Model
+from custom_envs.envs import BaseEnvironment
 
 __author__ = "Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>"
 
-from custom_envs import load_data
-from custom_envs.utils import shuffle, to_onehot, softmax, cross_entropy
-from custom_envs.model import Model
 
-LOG_EVERY = 1000
-# SOURCE:
-# https://github.com/rlpy/rlpy/blob/master/rlpy/Domains/Acrobot.py
-
-class Optimize(core.Env):
+class Optimize(BaseEnvironment):
 
     """
     Summary:
-    The optimize environment requires an agent to reduce the 
+    The optimize environment requires an agent to reduce the
     objective function of a target neural network on some dataset by changing
     the weights. The action of the agent is added to the current weights of
     the target neural network. 
 
     Target Network:
-    The target network is a simple multiclass softmax classifier. The 
+    The target network is a simple multiclass softmax classifier. The
     neural network operations are written with numpy.
 
     action_space: The number of dimensions is equal to the number of
@@ -41,69 +37,60 @@ class Optimize(core.Env):
         'render.modes': []
     }
 
-    def __init__(self, data_set='iris'):
-        features, labels = load_data(data_set)
-        print(features.shape)
-        self.features = features
-        self.labels, num_of_labels = to_onehot(labels, 3)
-        self.feature_size = features.shape[-1]
-        self.model = Model(self.feature_size, num_of_labels)
-        self.H = 3
-        self.obj_list = np.zeros(self.H)
-        self.grad_list = np.zeros((self.H, self.feature_size, num_of_labels))
-        self.W_list = np.zeros_like(self.grad_list)
-        self.N = 2*self.model.size + 1
-        self.observation_space = spaces.Box(low=-1e3, high=1e3, 
-                                            shape=(self.N,), dtype=np.float32)
-        self.action_space = spaces.Box(low=-1e3, high=1e3,
-                                       shape=(self.model.size,),
-                                       dtype=np.float32)
-        self.steps = 1
+    def __init__(self, data_set='iris', batch_size=None, n_of_steps=None):
+        self.sequence = load_data(data_set, batch_size)
+        num_of_labels = self.sequence.label_shape[0]
+        feature_size = self.sequence.feature_shape[0]
+        self.model = Model(feature_size, num_of_labels)
+        past_history = 3
+        self.loss_hist = np.zeros(past_history)
+        self.grad_hist = np.zeros((past_history, feature_size,
+                                   num_of_labels))
+        self.wght_hist = np.zeros(self.grad_hist.shape)
+        state_size = 2*self.model.size + 1
+        self.observation_space = Box(low=-1e3, high=1e3, dtype=np.float32,
+                                     shape=(state_size,))
+        self.action_space = Box(low=-1e3, high=1e3, dtype=np.float32,
+                                shape=(self.model.size,))
         self.seed()
-        self.num_of_labels = num_of_labels
-        self.global_steps = 0
-        self.seed_no = 0
 
-    def seed(self, seed=None):
-        self.seed_no = seed
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+    def _reset(self):
+        #shuffle(self.features, self.labels, np_random=self.np_random)
+        self.loss_hist.fill(0)
+        self.grad_hist.fill(0)
+        self.wght_hist.fill(0)
+        self.model.reset(npr)
+        return np.concatenate([self.wght_hist[0].ravel(), self.loss_hist[[0]],
+                               self.grad_hist[0].ravel()])
 
-    def reset(self):
-        shuffle(self.features, self.labels, np_random=self.np_random)
-        self.obj_list.fill(0)
-        self.grad_list.fill(0)
-        self.W_list.fill(0)
-        self.steps = 1
-        I = 0
-        self.model.reset(self.np_random)
-        return np.concatenate([self.W_list[I].ravel(), self.obj_list[[I]],
-                               self.grad_list[I].ravel()])
+    def _step(self, a):
+        idx = self.current_step % len(self.loss_hist)
 
-    def step(self, a):
-        self.global_steps += 1
-        I = self.steps % self.H
+        features, labels = self.sequence[0]
+        self.model.set_weights(self.model.weights -
+                               a.reshape((-1, self.wght_hist.shape[-1])))
+        loss, grad, accu = self.model.compute_backprop(features, labels)
 
-        labels = self.labels
-        features = self.features
+        grad = grad / len(features)
 
-        self.model.set_weights(self.model.weights - a.reshape((-1, self.num_of_labels)))
-        objective, gradient, accuracy = self.model.compute_backprop(features, labels)
+        np.divide(loss - self.loss_hist[idx-1],
+                  self.loss_hist[idx-1] + 0.1, out=self.loss_hist[[idx]])
+        np.divide(grad, np.abs(self.grad_hist[idx-1]) + 1,
+                  out=self.grad_hist[idx])
+        np.divide(np.abs(self.wght_hist[idx-1] - self.wght_hist[idx-2]),
+                  np.abs(self.wght_hist[idx] - self.wght_hist[idx-1]) + 0.1,
+                  out=self.wght_hist[idx])
 
-        self.obj_list[I] = (objective - self.obj_list[I-1]) / (self.obj_list[I-1] + 0.1)
-        self.grad_list[I] = gradient / (np.abs(self.grad_list[I-1])+1)
-        self.W_list[I] = np.abs(self.W_list[I-1] - self.W_list[I-2])/(np.abs(self.W_list[I] - self.W_list[I-1]) + 0.1)
-        self.steps += 1
-
-        state = np.concatenate([self.W_list[I].ravel(), self.obj_list[[I]],
-                                self.grad_list[I].ravel()])
+        state = np.concatenate([self.wght_hist[idx].ravel(),
+                                self.loss_hist[[idx]],
+                                self.grad_hist[idx].ravel()])
+        reward = -loss
         terminal = self._terminal()
-        reward = -objective
-        objective, _, accuracy = self.model.compute_backprop(self.features, self.labels)
-        return state, reward, terminal, {'objective': objective, 'accuracy': accuracy}
+        info = {'objective': loss, 'accuracy': accu}
+        return state, reward, terminal, info
 
     def _terminal(self):
-        return self.steps >= 40
+        return self.current_step > 40
 
     def render(self, mode='human'):
         pass
@@ -112,11 +99,15 @@ class Optimize(core.Env):
         pass
 
 
-if __name__ == '__main__':
+def __main():
     from stable_baselines.ppo2 import PPO2
     from stable_baselines.common.policies import MlpPolicy
     from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
     from stable_baselines.common.vec_env import DummyVecEnv
     env = DummyVecEnv([Optimize])
     agent = PPO2(MlpPolicy, env, verbose=1)
-    agent.learn(total_timesteps=10**5)
+    agent.learn(total_timesteps=10**4)
+
+
+if __name__ == '__main__':
+    __main()
