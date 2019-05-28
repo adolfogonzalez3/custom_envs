187c94c2200b3127506bdc488206f704fbd231e1
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-28 18:50:05 -0500

    Updated tool

--- run_exp_single.py
+++ run_exp_single.py
@@ -1,6 +1,9 @@
 
+import json
 import shutil
 import logging
+import sqlite3
+import concurrent.futures as confuture
 
 from time import time
 from pathlib import Path
@@ -15,17 +18,20 @@ from stable_baselines import ddpg
 from stable_baselines.bench import Monitor
 from stable_baselines import PPO2, A2C, DDPG
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines.common.misc_util import set_global_seeds
 
-from custom_envs.utils import create_env
+from custom_envs.utils.utils_common import create_env
+from custom_envs.envs.optimize import Optimize
 
 ENV_NAMES = ('Optimize-v0', 'OptLR-v0', 'OptLRs-v0')
 LOGGER = logging.getLogger(__name__)
 
+
 def run_agent(envs, alg, learning_rate, gamma, seed, path):
     set_global_seeds(seed)
     dummy_env = DummyVecEnv(envs)
+    #dummy_env = SubprocVecEnv(envs)
     if alg == 'PPO':
         model = PPO2(MlpPolicy, dummy_env, gamma=gamma,
                      learning_rate=learning_rate, verbose=1)
@@ -47,17 +53,19 @@ def run_agent(envs, alg, learning_rate, gamma, seed, path):
 
 def run_experiment(env_name, alg, learning_rate, gamma, seed, save_to):
     expr_id = (env_name, alg, learning_rate, gamma, seed)
-    begin = time()
-    num_of_envs = 1 if alg == 'DDPG' else 32
+    num_of_envs = 1 if alg == 'DDPG' else 1
     task_name = '{}-{:.4f}-{:.4f}-{:d}'.format(alg, learning_rate, gamma, seed)
     save_to = Path(save_to, env_name, task_name).resolve()
     with TemporaryDirectory() as tmpdir:
         log_dir = Path(tmpdir, task_name)
         log_dir.mkdir(parents=True)
+        with (log_dir / 'hyperparams.json').open('wt') as js_file:
+            json.dump({'env_name': env_name, 'alg': alg, 'gamma': gamma,
+                       'learning_rate': learning_rate, 'seed': seed}, js_file)
         save_path = str(log_dir / 'model')
         envs = create_env(env_name, log_dir, num_of_envs)
         for i, env in enumerate(envs):
-            env.seed(seed + i)
+            env.seed(seed + i*num_of_envs)
         envs_callable = [partial(lambda e: e, env) for env in envs]
         try:
             run_agent(envs_callable, alg, learning_rate, gamma, seed,
@@ -66,9 +74,11 @@ def run_experiment(env_name, alg, learning_rate, gamma, seed, save_to):
         except RuntimeError as error:
             LOGGER.error('%s, %s', error, expr_id)
         finally:
-            for env in envs:
-                env.close()
-    return expr_id, time() - begin
+            #for env in envs:
+            #    env.close()
+            pass
+    return expr_id
+
 
 def main_single_task():
     import sys
@@ -79,6 +89,7 @@ def main_single_task():
     seed = int(seed)
     path = Path(save_to).resolve()
     run_experiment(env_name, alg, lr, gamma, seed, save_to)
+
 
 def main_database(filename_db, filepath_exp):
     with sqlite3.connect(filename_db) as conn:
@@ -97,11 +108,12 @@ def main_database(filename_db, filepath_exp):
                                   'and seed=?'), args)
                 print('Finished: ', args, ' in ', time_elapsed)
 
+
 if __name__ == '__main__':
     import argparse
     main_single_task()
-    #create_tasks_file('tasks.csv')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('type', choices=['csv', 'sqlite3'])
+    # create_tasks_file('tasks.csv')
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('type', choices=['csv', 'sqlite3'])
 
-    args = parser.parse_args()
+    #args = parser.parse_args()
