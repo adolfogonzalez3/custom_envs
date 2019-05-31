4a76ff320a6035f52ce0c449a4eea8b1f9c11ba4
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-31 17:54:59 -0500

    Removed unnecessary files

--- custom_envs/multiagent/multiagent.py
+++
@@ -1,7 +0,0 @@
-
-class MultiAgent:
-    def __init__(self, agent_fns, env, seed=None):
-        env = gym.make(env_name)
-        env.seed(seed)
-        self.envs = env
-        self.agents = [Thread(fn)]
