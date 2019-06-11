c0653d46b438565e95228668d44d27f7c6d3169d
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-06-11 01:31:30 -0500

    Added a utils file for math ops

---
+++ tests/utils/test_utils_math.py
@@ -0,0 +1,79 @@
+'''Tests utils_common module.'''
+import pytest
+import numpy as np
+import numpy.random as npr
+
+import custom_envs.utils.utils_math as utils
+
+BATCH_SIZES = tuple(2**np.arange(5))
+MAGNITUDE = tuple(range(5))
+NUM_OF_SEED = 5
+NUM_OF_ARRAYS = 5
+SHAPES = tuple((2**i, i + 2) for i in range(5))
+
+
+@pytest.mark.parametrize("seed", range(NUM_OF_SEED))
+@pytest.mark.parametrize("shape", SHAPES)
+def test_cross_entropy(seed, shape):
+    '''Tests cross_entropy.'''
+    npr.seed(seed)
+    array = utils.softmax(npr.uniform(size=shape))
+    labels = npr.uniform(size=shape)
+    labels = labels / np.expand_dims(np.mean(labels, axis=1), axis=1)
+    cost = utils.cross_entropy(array, labels)
+    assert cost >= 0
+    assert isinstance(cost, float)
+
+
+@pytest.mark.parametrize("seed", range(NUM_OF_SEED))
+@pytest.mark.parametrize("shape", SHAPES)
+def test_mse(seed, shape):
+    '''Tests mse.'''
+    npr.seed(seed)
+    array = npr.uniform(size=shape)
+    array = array - np.min(array) / np.ptp(array)
+    labels = npr.uniform(size=shape)
+    cost = utils.mse(array, labels)
+    assert cost >= 0
+    assert isinstance(cost, float)
+
+
+@pytest.mark.parametrize("seed", range(NUM_OF_SEED))
+@pytest.mark.parametrize("magnitude", MAGNITUDE)
+@pytest.mark.parametrize("shape", SHAPES)
+def test_softmax(seed, magnitude, shape):
+    '''Tests softmax.'''
+    npr.seed(seed)
+    low = -10**magnitude
+    high = 10**magnitude
+    array = npr.uniform(low, high, size=shape).tolist()
+    array = utils.softmax(array)
+    summation = np.sum(array, axis=1)
+    assert np.all(array >= 0)
+    assert np.all(array <= 1)
+    assert np.all(np.isclose(summation, 1))
+
+
+@pytest.mark.parametrize("seed", range(NUM_OF_SEED))
+@pytest.mark.parametrize("magnitude", MAGNITUDE)
+@pytest.mark.parametrize("shape", SHAPES)
+def test_sigmoid(seed, magnitude, shape):
+    '''Tests sigmoid.'''
+    npr.seed(seed)
+    low = -10**magnitude
+    high = 10**magnitude
+    array = npr.randint(low, high, size=shape)
+    array = utils.sigmoid(array)
+    assert np.all(array >= 0)
+    assert np.all(array <= 1)
+
+
+@pytest.mark.parametrize("seed", range(NUM_OF_SEED))
+@pytest.mark.parametrize("shape", SHAPES)
+def test_normalize_bound(seed, shape):
+    '''Test that normalize function normalizes within 0 and 1.'''
+    npr.seed(seed)
+    array = npr.randn(*shape)
+    array = utils.normalize(array)
+    assert np.all(array >= 0)
+    assert np.all(array <= 1)
