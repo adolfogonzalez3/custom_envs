6f50d3fbc215a8f2834def07bce3acf9c6218c1d
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-06-12 16:02:49 -0500

    Updated experiment scripts

--- eval_exp.py
+++ eval_exp.py
@@ -1,46 +1,26 @@
 '''Module for evaluating learned agents against different environments.'''
-import os
-import subprocess
+import argparse
 from pathlib import Path
-from collections import Counter, defaultdict
-from itertools import product, cycle, chain
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-from tqdm import trange
+from itertools import chain
 
 import gym
-from stable_baselines.bench import Monitor
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import trange
+import tensorflow as tf
 from stable_baselines import PPO2, A2C, DDPG
-from stable_baselines.common.misc_util import set_global_seeds
 
-import custom_envs
+import custom_envs.utils.utils_plot as utils_plot
 from custom_envs.utils.utils_common import enzip
 from custom_envs.data import load_data
 
-def plot(axis_obj, sequence, **kwargs):
-    axis_obj.plot(range(len(sequence)), sequence, **kwargs)
-
-def fill_between(axis_obj, mean, std, **kwargs):
-    axis_obj.fill_between(range(len(mean)), mean + std, mean - std, **kwargs)
-
-def add_legend(axis):
-    chartBox = axis.get_position()
-    axis.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.8, chartBox.height])
-    axis.legend(loc='upper center', bbox_to_anchor=(1.2, 0.8), shadow=True, ncol=1)
-
 
 def task(path, seed, batch_size=None):
+    '''Run the agent on a data set.'''
     alg, learning_rate, gamma, _ = path.name.split('-')
     learning_rate = float(learning_rate)
     gamma = float(gamma)
     env_name = 'Optimize-v0'
-    #env_name = 'OptimizeCorrect-v0'
-    #env_name = 'OptLRs-v0'
     save_path = path / 'model.pkl'
     env = gym.make(env_name, data_set='mnist', batch_size=batch_size)
     sequence = load_data('mnist')
@@ -54,17 +34,13 @@ def task(path, seed, batch_size=None):
     elif alg == 'DDPG':
         model = DDPG.load(save_path)
     env.seed(seed)
-    obs = env.reset()
-    terminal = False
+    states = env.reset()
     infos = []
     epoch_no = 0
     current_sample_no = 0
     while epoch_no < 40:
-    #while not terminal:
-    #for _ in range(75000):
-    #for _ in range(1875): # One epoch on mnist for minibatch size = 32
-        action, *_ = model.predict(obs)
-        obs, reward, terminal, info = env.step(action)
+        action, *_ = model.predict(states)
+        states, reward, _, info = env.step(action)
         info['step'] = len(infos)
         info['reward'] = reward
         info['seed'] = seed
@@ -79,16 +55,13 @@ def task(path, seed, batch_size=None):
             epoch_no += 1
     return infos
 
-def task_lr(seed, batch_size=None):
-    import tensorflow as tf
-    import tensorflow.keras as keras
 
-    from custom_envs import load_data
+def task_lr(seed, batch_size=None):
+    '''Train a logistic classification model.'''
     sequence = load_data('mnist')
     features = sequence.features
     labels = sequence.labels
     batch_size = len(features) if batch_size is None else batch_size
-
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(labels.shape[-1],
                                     input_shape=features.shape[1:],
@@ -98,20 +71,20 @@ def task_lr(seed, batch_size=None):
                   metrics=['accuracy'])
     hist = model.fit(features, labels, epochs=40, verbose=0,
                      batch_size=batch_size).history
-    info = [{'epoch': epoch, 'loss': lss, 'accuracy': acc, 'seed': seed}
+    return [{'epoch': epoch, 'loss': lss, 'accuracy': acc, 'seed': seed}
             for epoch, lss, acc in enzip(hist['loss'], hist['acc'])]
-    return info
+
 
 def run_multi(trials=10, batch_size=None):
-    import argparse
+    '''Run both agent evaluationg and logistic classification training.'''
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="The directory to save files to.")
     args = parser.parse_args()
     path = Path(args.path)
-    infos = list(chain.from_iterable([task(path, i, batch_size)
+    infos = list(chain.from_iterable([task(path, i, batch_size=batch_size)
                                       for i in trange(trials)]))
     dataframe_rl = pd.DataFrame(infos)
-    infos = list(chain.from_iterable([task_lr(i, batch_size)
+    infos = list(chain.from_iterable([task_lr(i, batch_size=batch_size)
                                       for i in trange(trials)]))
     dataframe_lc = pd.DataFrame(infos)
     mean_rl = dataframe_rl.groupby('epoch').mean()
@@ -119,31 +92,36 @@ def run_multi(trials=10, batch_size=None):
     mean_lc = dataframe_lc.groupby('epoch').mean()
     std_lc = dataframe_lc.groupby('epoch').std()
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title('Performance on MNIST data set')
-    ax.set_ylabel('loss')
-    ax.set_xlabel('epoch')
-    plot(ax, mean_rl['objective'], label='RL')
-    fill_between(ax, mean_rl['objective'], std_rl['objective'], alpha=0.1,
-                 label='RL')
-    plot(ax, mean_lc['loss'], label='Logistic Classification')
-    fill_between(ax, mean_lc['loss'], std_lc['loss'], alpha=0.1,
-                 label='Logistic Classification')
-    add_legend(ax)
+    axis = fig.add_subplot(111)
+    pyplot_attr = {
+        'title': 'Performance on MNIST data set',
+        'ylabel': 'Loss',
+        'xlabel': 'Epoch',
+    }
+    utils_plot.set_attributes(axis, pyplot_attr)
+    utils_plot.plot_sequence(axis, mean_rl['objective'], label='RL')
+    utils_plot.fill_between(axis, mean_rl['objective'], std_rl['objective'],
+                            alpha=0.1, label='RL')
+    utils_plot.plot_sequence(axis, mean_lc['loss'],
+                             label='Logistic Classification')
+    utils_plot.fill_between(axis, mean_lc['loss'], std_lc['loss'], alpha=0.1,
+                            label='Logistic Classification')
+    utils_plot.add_legend(axis)
 
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title('Performance on MNIST data set')
-    ax.set_ylabel('accuracy')
-    ax.set_xlabel('epoch')
-    plot(ax, mean_rl['accuracy'], label='RL')
-    fill_between(ax, mean_rl['accuracy'], std_rl['accuracy'], alpha=0.1,
-                 label='RL')
-    plot(ax, mean_lc['accuracy'], label='Logistic Classification')
-    fill_between(ax, mean_lc['accuracy'], std_lc['accuracy'], alpha=0.1,
-                 label='Logistic Classification')
-    add_legend(ax)
+    axis = fig.add_subplot(111)
+    pyplot_attr['ylabel'] = 'Accuracy'
+    utils_plot.set_attributes(axis, pyplot_attr)
+    utils_plot.plot_sequence(axis, mean_rl['accuracy'], label='RL')
+    utils_plot.fill_between(axis, mean_rl['accuracy'], std_rl['accuracy'],
+                            alpha=0.1, label='RL')
+    utils_plot.plot_sequence(axis, mean_lc['accuracy'],
+                             label='Logistic Classification')
+    utils_plot.fill_between(axis, mean_lc['accuracy'], std_lc['accuracy'],
+                            alpha=0.1, label='Logistic Classification')
+    utils_plot.add_legend(axis)
     plt.show()
 
+
 if __name__ == '__main__':
-    run_multi(10, batch_size=2048)
+    run_multi(1, batch_size=None)
--- eval_multiexp.py
+++ eval_multiexp.py
@@ -1,61 +1,34 @@
-
-import os
-import subprocess
+'''Module for evaluating learned agents against different environments.'''
+import argparse
 from threading import Thread
 from pathlib import Path
 from functools import partial
-from collections import Counter, defaultdict
-from itertools import product, cycle, chain
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from itertools import chain
 
-import matplotlib.pyplot as plt
+import tensorflow as tf
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 from tqdm import trange
-
-import gym
-from stable_baselines.bench import Monitor
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2, A2C, DDPG
-from stable_baselines.common.misc_util import set_global_seeds
 
-from custom_envs.multiagent import EnvironmentInSync, MultiEnvServer
-from custom_envs.utils.utils_venv import SubprocVecEnv, ThreadVecEnv
+import custom_envs.utils.utils_plot as utils_plot
+from custom_envs.multiagent import MultiEnvServer
+from custom_envs.utils.utils_venv import ThreadVecEnv
 from custom_envs.envs.multioptlrs import MultiOptLRs
-
-import custom_envs
 from custom_envs.utils.utils_common import enzip
 from custom_envs.data import load_data
 
 
-def plot(axis_obj, sequence, **kwargs):
-    axis_obj.plot(range(len(sequence)), sequence, **kwargs)
-
-
-def fill_between(axis_obj, mean, std, **kwargs):
-    axis_obj.fill_between(range(len(mean)), mean + std, mean - std, **kwargs)
-
-
-def add_legend(axis):
-    chartBox = axis.get_position()
-    axis.set_position(
-        [chartBox.x0, chartBox.y0, chartBox.width*0.8, chartBox.height])
-    axis.legend(loc='upper center', bbox_to_anchor=(
-        1.2, 0.8), shadow=True, ncol=1)
-
-
 def run_handle(env):
+    '''Run handle requests until complete.'''
     data = 0
     while data is not None:
         data = env.handle_requests()
 
 
 def task(path, seed, batch_size=None):
-    alg, learning_rate, gamma, _ = path.name.split('-')
-    learning_rate = float(learning_rate)
-    gamma = float(gamma)
-    env_name = 'MultiOptLRs-v0'
+    '''Run the agent on a data set.'''
+    alg, *_ = path.name.split('-')
     save_path = path / 'model.pkl'
     env = MultiOptLRs(data_set='mnist', batch_size=batch_size)
     sequence = load_data('mnist')
@@ -75,18 +48,13 @@ def task(path, seed, batch_size=None):
         model = DDPG.load(save_path)
     taskrun = Thread(target=run_handle, args=[main_environment])
     taskrun.start()
-    # dummy_env.seed(seed)
-    obs = dummy_env.reset()
-    terminals = [False]
+    states = dummy_env.reset()
     info_list = []
     epoch_no = 0
     current_sample_no = 0
     while epoch_no < 40:
-    #while not all(terminals):
-    # for _ in range(75000):
-    # for _ in range(1800):
-        action, *_ = model.predict(obs)
-        states, rewards, terminals, infos = dummy_env.step(action)
+        action, *_ = model.predict(states)
+        states, rewards, _, infos = dummy_env.step(action)
         info = infos[0]
         info['step'] = len(info_list)
         info['reward'] = sum(rewards)
@@ -105,15 +73,12 @@ def task(path, seed, batch_size=None):
     return info_list
 
 
-def task_lr(seed):
-    import tensorflow as tf
-    import tensorflow.keras as keras
-
-    from custom_envs import load_data
+def task_lr(seed, batch_size=None):
+    '''Train a logistic classification model.'''
     sequence = load_data('mnist')
     features = sequence.features
     labels = sequence.labels
-
+    batch_size = len(features) if batch_size is None else batch_size
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(labels.shape[-1],
                                     input_shape=features.shape[1:],
@@ -121,52 +86,59 @@ def task_lr(seed):
     model.compile(tf.train.GradientDescentOptimizer(0.1),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    hist = model.fit(features, labels, epochs=40, verbose=0).history
-    info = [{'epoch': epoch, 'loss': lss, 'accuracy': acc, 'seed': seed}
+    hist = model.fit(features, labels, epochs=40, verbose=0,
+                     batch_size=batch_size).history
+    return [{'epoch': epoch, 'loss': lss, 'accuracy': acc, 'seed': seed}
             for epoch, lss, acc in enzip(hist['loss'], hist['acc'])]
 
 
-def run_multi(trials=10):
-    import argparse
+def run_multi(trials=10, batch_size=None):
+    '''Run both agent evaluationg and logistic classification training.'''
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="The directory to save files to.")
     args = parser.parse_args()
     path = Path(args.path)
-    infos = list(chain.from_iterable([task(path, i) for i in trange(trials)]))
+    infos = list(chain.from_iterable([task(path, i, batch_size=batch_size)
+                                      for i in trange(trials)]))
     dataframe_rl = pd.DataFrame(infos)
-    infos = list(chain.from_iterable([task_lr(i) for i in trange(trials)]))
+    infos = list(chain.from_iterable([task_lr(i, batch_size=batch_size)
+                                      for i in trange(trials)]))
     dataframe_lc = pd.DataFrame(infos)
     mean_rl = dataframe_rl.groupby('epoch').mean()
     std_rl = dataframe_rl.groupby('epoch').std()
     mean_lc = dataframe_lc.groupby('epoch').mean()
     std_lc = dataframe_lc.groupby('epoch').std()
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title('Performance on MNIST data set')
-    ax.set_ylabel('loss')
-    ax.set_xlabel('epoch')
-    plot(ax, mean_rl['objective'], label='RL')
-    fill_between(ax, mean_rl['objective'], std_rl['objective'], alpha=0.1,
-                 label='RL')
-    plot(ax, mean_lc['loss'], label='Logistic Classification')
-    fill_between(ax, mean_lc['loss'], std_lc['loss'], alpha=0.1,
-                 label='Logistic Classification')
-    add_legend(ax)
+    axis = fig.add_subplot(111)
+    pyplot_attr = {
+        'title': 'Performance on MNIST data set',
+        'ylabel': 'Loss',
+        'xlabel': 'Epoch',
+    }
+    utils_plot.set_attributes(axis, pyplot_attr)
+    utils_plot.plot_sequence(axis, mean_rl['objective'], label='RL')
+    utils_plot.fill_between(axis, mean_rl['objective'], std_rl['objective'],
+                            alpha=0.1, label='RL')
+    utils_plot.plot_sequence(axis, mean_lc['loss'],
+                             label='Logistic Classification')
+    utils_plot.fill_between(axis, mean_lc['loss'], std_lc['loss'], alpha=0.1,
+                            label='Logistic Classification')
+    utils_plot.add_legend(axis)
 
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title('Performance on MNIST data set')
-    ax.set_ylabel('accuracy')
-    ax.set_xlabel('epoch')
-    plot(ax, mean_rl['accuracy'], label='RL')
-    fill_between(ax, mean_rl['accuracy'], std_rl['accuracy'], alpha=0.1,
-                 label='RL')
-    plot(ax, mean_lc['accuracy'], label='Logistic Classification')
-    fill_between(ax, mean_lc['accuracy'], std_lc['accuracy'], alpha=0.1,
-                 label='Logistic Classification')
-    add_legend(ax)
+    axis = fig.add_subplot(111)
+    pyplot_attr['ylabel'] = 'Accuracy'
+    utils_plot.set_attributes(axis, pyplot_attr)
+    utils_plot.plot_sequence(axis, mean_rl['accuracy'], label='RL')
+    utils_plot.fill_between(axis, mean_rl['accuracy'], std_rl['accuracy'],
+                            alpha=0.1, label='RL')
+    utils_plot.plot_sequence(axis, mean_lc['accuracy'],
+                             label='Logistic Classification')
+    utils_plot.fill_between(axis, mean_lc['accuracy'], std_lc['accuracy'],
+                            alpha=0.1, label='Logistic Classification')
+    utils_plot.add_legend(axis)
     plt.show()
 
 
 if __name__ == '__main__':
-    run_multi(10)
+    run_multi(1, batch_size=None)
