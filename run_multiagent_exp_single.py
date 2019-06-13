3c6b7fa18126476d73045a601d107443e56317ef
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-06-12 19:05:04 -0500

    Updated experiment scripts

--- run_multiagent_exp_single.py
+++ run_multiagent_exp_single.py
@@ -1,34 +1,39 @@
-'''Run a single agent experiment.'''
+'''Run an multi agent experiment.'''
 import sys
 import shutil
 import logging
 import argparse
-
 from pathlib import Path
+from threading import Thread
 from functools import partial
 from tempfile import TemporaryDirectory
 
-import gym
 import pandas as pd
 import tensorflow as tf
-from stable_baselines import ddpg
+import stable_baselines.ddpg as ddpg
+from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import PPO2, A2C, DDPG
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy
 from stable_baselines.common.misc_util import set_global_seeds
 
 import custom_envs.utils.utils_file as utils_file
+from custom_envs.multiagent import MultiEnvServer
 from custom_envs.utils.utils_logging import Monitor
 from custom_envs.utils.utils_venv import ThreadVecEnv
+from custom_envs.envs.multioptlrs import MultiOptLRs
 
 LOGGER = logging.getLogger(__name__)
 
 
-def run_agent(envs, alg, learning_rate, gamma, seed, path):
+def run_agent(envs, parameters):
     '''Train an agent.'''
-    set_global_seeds(seed)
+    alg = parameters['alg']
+    learning_rate = parameters['learning_rate']
+    gamma = parameters['gamma']
+    model_path = parameters['model_path']
+    set_global_seeds(parameters.get('seed'))
     dummy_env = ThreadVecEnv(envs)
     if alg == 'PPO':
-        model = PPO2(MlpLstmPolicy, dummy_env, gamma=gamma,
+        model = PPO2(MlpPolicy, dummy_env, gamma=gamma,
                      learning_rate=learning_rate, verbose=1)
     elif alg == 'A2C':
         model = A2C(MlpPolicy, dummy_env, gamma=gamma,
@@ -37,46 +42,53 @@ def run_agent(envs, alg, learning_rate, gamma, seed, path):
         model = DDPG(ddpg.MlpPolicy, dummy_env, gamma=gamma, verbose=1,
                      actor_lr=learning_rate/10, critic_lr=learning_rate)
     try:
-        model.learn(total_timesteps=10**6)
+        model.learn(total_timesteps=parameters.get('total_timesteps', 10**6))
     except tf.errors.InvalidArgumentError:
         LOGGER.error('Possible Nan, %s', str((alg, learning_rate, gamma)))
     finally:
         dummy_env.close()
-        model.save(str(path))
+        model.save(str(model_path))
+
+
+def run_handle(env):
+    '''Run handle until all experiments end.'''
+    data = 0
+    while data is not None:
+        data = env.handle_requests()
 
 
 def run_experiment(parameters):
     '''Set up and run an experiment.'''
     repository_path = Path(__file__).parent
-    env_name = parameters['env_name']
-    alg = parameters['alg']
-    learning_rate = parameters['learning_rate']
-    gamma = parameters['gamma']
-    seed = parameters['seed']
     save_to = parameters['path']
-    num_of_envs = 1 if alg == 'DDPG' else parameters.get('num_of_envs', 32)
-    task_name = '{}-{:.4f}-{:.4f}-{:d}'.format(alg, learning_rate, gamma, seed)
-    save_to = Path(save_to, task_name).resolve()
+    task_name = '{alg}-{learning_rate:.4f}-{gamma:.4f}-{seed:d}'
+    task_name = task_name.format(**parameters)
+    save_to = Path(save_to, task_name)
     with TemporaryDirectory() as tmpdir:
         log_dir = Path(tmpdir, task_name)
         log_dir.mkdir(parents=True)
-        parameters['num_of_envs'] = num_of_envs
         parameters['commit'] = utils_file.get_commit_hash(repository_path)
         utils_file.save_json(parameters, log_dir / 'hyperparams.json')
-        model_path = str(log_dir / 'model.pkl')
+        env = MultiOptLRs(data_set='iris',
+                          batch_size=parameters.get('batch_size', 32))
+        env.seed(parameters.get('seed'))
+        parameters['model_path'] = str(log_dir / 'model.pkl')
         log_path = str(log_dir / 'monitor_{:d}')
-        envs_callable = [partial(Monitor,
-                                 gym.make(env_name, data_set='mnist',
-                                          batch_size=32,
-                                          version=3),
-                                 log_path.format(i),
-                                 allow_early_resets=True,
-                                 info_keywords=('objective', 'accuracy'),
-                                 chunk_size=1)
-                         for i in range(num_of_envs)]
+        main_environment = MultiEnvServer(env)
+        env_callable = [partial(Monitor, subenv, log_path.format(i),
+                                allow_early_resets=True,
+                                info_keywords=('objective', 'accuracy'),
+                                chunk_size=10)
+                        for i, subenv in
+                        enumerate(main_environment.sub_environments.values())]
         try:
-            run_agent(envs_callable, alg, learning_rate, gamma, seed,
-                      model_path)
+            task = Thread(target=run_handle, args=[main_environment],
+                          daemon=True)
+            taskrun = Thread(target=run_agent, args=[env_callable, parameters])
+            task.start()
+            taskrun.start()
+            taskrun.join()
+            task.join()
         except RuntimeError as error:
             LOGGER.error('%s, %s', error, parameters)
         finally:
@@ -102,8 +114,6 @@ def loop_over_json_file():
     args = parser.parse_args()
     dataframe = pd.read_json(args.file_path, orient='records', lines=True)
     parameters_list = dataframe.to_dict(orient='records')
-    # with ProcessPoolExecutor(1) as executor:
-    #    executor.map(run_experiment, parameters_list)
     for parameters in parameters_list[:1]:
         print(parameters)
         run_experiment(parameters)
@@ -111,8 +121,9 @@ def loop_over_json_file():
 
 def main():
     '''Test run the code.'''
-    parameters = {"alg": "PPO", "env_name": "OptLRs-v0", "gamma": 0.9,
-                  "learning_rate": 0.001, "path": "results_iris2", "seed": 0}
+    parameters = {"alg": "PPO", "env_name": "MultiOptLRs-v0", "gamma": 0.9,
+                  "learning_rate": 0.001, "path": "results_iriss", "seed": 0,
+                  "total_timesteps": 10**4}
     run_experiment(parameters)
 
 
