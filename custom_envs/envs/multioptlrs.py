d84c180efd751946364653627ec168d9c810b118
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-06-15 15:25:51 -0500

    Updated multiagent environments Reason: The classes spaces were not complaint with OpenAI gym's specs. In order to ensure that the environments are flexible and compatible with any future algorithm the spaces were made to be Dict. Another Change is that the classes now use the History class which simpliifies much of the code.

--- custom_envs/envs/multioptlrs.py
+++ custom_envs/envs/multioptlrs.py
@@ -3,11 +3,12 @@
 import numpy as np
 import numpy.random as npr
 
-from gym.spaces import Box
+from gym.spaces import Box, Dict
 
 from custom_envs import load_data
 from custom_envs.models import ModelNumpy as Model
 from custom_envs.envs import BaseMultiEnvironment
+from custom_envs.utils.utils_common import History
 
 # SOURCE:
 # https://github.com/rlpy/rlpy/blob/master/rlpy/Domains/Acrobot.py
@@ -36,135 +37,94 @@ class MultiOptLRs(BaseMultiEnvironment):
 
     AGENT_FMT = 'parameter-{:d}'
 
-    def __init__(self, data_set='iris', batch_size=None, version=1):
+    def __init__(self, data_set='iris', batch_size=None, version=1,
+                 max_batches=400, max_history=5):
         super().__init__()
         self.sequence = load_data(data_set, batch_size)
         num_of_labels = self.sequence.label_shape[0]
         feature_size = self.sequence.feature_shape[0]
+        model_shape = (feature_size, num_of_labels)
         self.model = Model(feature_size, num_of_labels)
-        past_history = 5
-        self.loss_hist = np.zeros((past_history, 1))
-        self.grad_hist = np.zeros((past_history, feature_size,
-                                   num_of_labels))
-        self.wght_hist = np.zeros(self.grad_hist.shape)
+        self.history = History(max_history, losses=(), gradients=model_shape,
+                               weights=model_shape)
         if version == 0:
+            self.adjusted_history = History(1, gradients=model_shape)
             state_size = 1
         elif version == 1:
+            self.adjusted_history = History(1, losses=(),
+                                            gradients=model_shape)
             state_size = 2
         elif version == 2:
-            state_size = 2 + 1
+            self.adjusted_history = History(1, weights=model_shape,
+                                            losses=(), gradients=model_shape)
+            state_size = 3
         elif version == 3:
-            state_size = 2*past_history
-        self.observation_spaces = {MultiOptLRs.AGENT_FMT.format(i):
-                                   Box(low=-1e3, high=1e3,
+            self.adjusted_history = History(max_history, weights=model_shape,
+                                            losses=(), gradients=model_shape)
+            state_size = 3*max_history
+        self.observation_spaces = Dict({MultiOptLRs.AGENT_FMT.format(i):
+                                        Box(low=-1e3, high=1e3,
+                                            dtype=np.float32,
+                                            shape=(state_size,))
+                                        for i in range(self.model.size)})
+        self.action_spaces = Dict({MultiOptLRs.AGENT_FMT.format(i):
+                                   Box(low=0., high=1.,
                                        dtype=np.float32,
-                                       shape=(state_size,))
-                                   for i in range(self.model.size)}
-        self.action_spaces = {MultiOptLRs.AGENT_FMT.format(i):
-                              Box(low=0., high=1.,
-                                  dtype=np.float32,
-                                  shape=(1,))
-                              for i in range(self.model.size)}
+                                       shape=(1,))
+                                   for i in range(self.model.size)})
         self.seed()
-        self.adjusted_loss_hist = np.zeros(self.loss_hist.shape)
-        self.adjusted_grad_hist = np.zeros(self.grad_hist.shape)
-        self.adjusted_wght_hist = np.zeros(self.wght_hist.shape)
+        self.max_history = max_history
+        self.max_batches = max_batches
         self.version = version
 
     def base_reset(self):
-        self.loss_hist.fill(0)
-        self.grad_hist.fill(0)
-        self.wght_hist.fill(0)
-        self.adjusted_loss_hist.fill(0)
-        self.adjusted_grad_hist.fill(0)
-        self.adjusted_wght_hist.fill(0)
+        self.history.reset()
+        self.adjusted_history.reset()
         self.model.reset(npr)
-        self.sequence.shuffle()
-        #self.sequence.features = np.roll(self.sequence.features, 1, axis=1)
-        if self.version == 0:
-            grad_flat = self.grad_hist[0].ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): gd
-                      for i, gd in enumerate(grad_flat)}
-        elif self.version == 1:
-            loss_flat = float(self.loss_hist[0])
-            grad_flat = self.grad_hist[0].ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (loss_flat, gd)
-                      for i, gd in enumerate(grad_flat)}
-        elif self.version == 2:
-            wght_flat = self.wght_hist[0].ravel()
-            loss_flat = float(self.loss_hist[0])
-            grad_flat = self.grad_hist[0].ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (wght, loss_flat, gd)
-                      for i, (wght, gd) in
-                      enumerate(zip(wght_flat, grad_flat))}
-        elif self.version == 3:
-            loss_flat = self.adjusted_loss_hist.ravel()
-            grad_flat = self.adjusted_grad_hist.ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (loss_flat, gd)
-                      for i, gd in enumerate(grad_flat)}
+        states = self.adjusted_history.build_multistate()
+        states = {MultiOptLRs.AGENT_FMT.format(i): list(values)
+                  for i, values in enumerate(states)}
         return states
 
-    def base_step(self, actions):
-        shape = (-1, self.wght_hist.shape[-1])
-        action = np.reshape([actions['parameter-{:d}'.format(i)]
+    def base_step(self, action):
+        shape = (-1, self.history['weights'].shape[-1])
+        action = np.reshape([action['parameter-{:d}'.format(i)]
                              for i in range(self.model.size)], shape)
-        idx = self.current_step % len(self.loss_hist)
 
         seq_idx = self.current_step % len(self.sequence)
+        if seq_idx == 0:
+            self.sequence.shuffle()
         features, labels = self.sequence[seq_idx]
         loss, grad, accu = self.model.compute_backprop(features, labels)
         self.model.set_weights(self.model.weights - grad*action)
         loss, grad, accu = self.model.compute_backprop(features, labels)
 
         grad = grad / len(features)
-        self.loss_hist[idx] = loss
-        self.grad_hist[idx] = grad
-        self.wght_hist[idx] = self.model.weights
+        self.history.append(losses=loss, gradients=grad,
+                            weights=self.model.weights)
+        past_losses = self.history['losses']
+        past_grads = self.history['gradients']
+        past_weights = self.history['weights']
 
-        if self.version == 3:
-            mean_loss = np.mean(self.loss_hist)
-            adjusted_loss = np.divide(loss - mean_loss, np.abs(mean_loss) + .1)
-            mean_grad = np.mean(self.grad_hist)
-            adjusted_grad = np.divide(grad, np.abs(mean_grad) + 1)
-            prev_wght = np.abs(self.wght_hist[idx-1] - self.wght_hist[idx-2])
-            abs_wght = np.abs(
-                self.wght_hist[idx] - self.wght_hist[idx-1]) + 0.1
-            adjusted_wght = np.divide(prev_wght, abs_wght)
-        else:
-            adjusted_loss = np.divide(loss - self.loss_hist[idx-1],
-                                      np.abs(self.loss_hist[idx-1]) + 0.1)
-            adjusted_grad = np.divide(grad, np.abs(self.grad_hist[idx-1]) + 1)
-            adjusted_grad = np.divide(grad, np.abs(self.grad_hist[idx-1]) + 1)
-            prev_wght = np.abs(self.wght_hist[idx-1] - self.wght_hist[idx-2])
-            abs_wght = np.abs(
-                self.wght_hist[idx] - self.wght_hist[idx-1]) + 0.1
-            adjusted_wght = np.divide(prev_wght, abs_wght)
-
-        self.adjusted_loss_hist[idx] = adjusted_loss
-        self.adjusted_grad_hist[idx] = adjusted_grad
-        self.adjusted_wght_hist[idx] = adjusted_wght
+        adjusted_loss = np.divide(past_losses[0] - past_losses[1],
+                                  np.abs(past_losses[1]) + 0.1)
+        adjusted_grad = np.divide(past_grads[0], np.abs(past_grads[1]) + 1)
+        prev_wght = np.abs(past_weights[1] - past_weights[2])
+        abs_wght = np.abs(past_weights[0] - past_weights[1])
+        adjusted_wght = np.divide(prev_wght, abs_wght + 0.1)
 
         if self.version == 0:
-            grad_flat = adjusted_grad.ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): gd
-                      for i, gd in enumerate(grad_flat)}
+            self.adjusted_history.append(gradients=adjusted_grad)
         elif self.version == 1:
-            loss_flat = float(adjusted_loss)
-            grad_flat = adjusted_grad.ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (loss_flat, gd)
-                      for i, gd in enumerate(grad_flat)}
-        elif self.version == 2:
-            wght_flat = adjusted_wght.ravel()
-            loss_flat = float(adjusted_loss)
-            grad_flat = adjusted_grad.ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (wght, loss_flat, gd)
-                      for i, (wght, gd) in
-                      enumerate(zip(wght_flat, grad_flat))}
-        elif self.version == 3:
-            loss_flat = self.adjusted_loss_hist.ravel()
-            grad_flat = self.adjusted_grad_hist.ravel()
-            states = {MultiOptLRs.AGENT_FMT.format(i): (loss_flat, gd)
-                      for i, gd in enumerate(grad_flat)}
+            self.adjusted_history.append(losses=adjusted_loss,
+                                         gradients=adjusted_grad)
+        elif self.version == 2 or self.version == 3:
+            self.adjusted_history.append(weights=adjusted_wght,
+                                         losses=adjusted_loss,
+                                         gradients=adjusted_grad)
+        state = self.adjusted_history.build_multistate()
+        states = {MultiOptLRs.AGENT_FMT.format(i): list(values)
+                  for i, values in enumerate(state)}
         reward = -float(adjusted_loss)
         rewards = {MultiOptLRs.AGENT_FMT.format(i): reward
                    for i in range(self.model.size)}
@@ -175,14 +135,20 @@ class MultiOptLRs(BaseMultiEnvironment):
             features = self.sequence.features
             labels = self.sequence.labels
             loss, _, accu = self.model.compute_backprop(features, labels)
-        info = {'objective': loss, 'accuracy': accu}
+        info = {'objective': loss, 'accuracy': accu,
+                'weights_mean': np.mean(self.model.weights),
+                'actions_mean': np.mean(action),
+                'states_mean': np.mean(state),
+                'grads_mean': np.mean(self.history['gradients']),
+                'loss_mean': np.mean(self.history['losses'])
+                }
         infos = {MultiOptLRs.AGENT_FMT.format(i): info
                  for i in range(self.model.size)}
 
         return states, rewards, terminals, infos
 
     def _terminal(self):
-        return self.current_step >= 400
+        return self.current_step >= self.max_batches
 
     def render(self, mode='human'):
         pass
--- tests/envs/test_multenv.py
+++ tests/envs/test_multenv.py
@@ -7,18 +7,19 @@ from custom_envs.envs import MULTI_AGENT_ENVIRONMENTS
 @pytest.mark.parametrize("environment_class", MULTI_AGENT_ENVIRONMENTS)
 def test_step(environment_class):
     environ = environment_class()
-    action = {name: i for i, name in enumerate(environ.action_spaces.keys())}
+    #action = {name: i for i, name in enumerate(environ.action_spaces.keys())}
+    action = environ.action_spaces.sample()
     states, rewards, terminals, infos = environ.step(action)
-    assert states.keys() == environ.action_spaces.keys()
-    assert rewards.keys() == environ.action_spaces.keys()
+    assert environ.observation_spaces.contains(states)
+    assert rewards.keys() == environ.action_spaces.spaces.keys()
     assert all(isinstance(reward, float) for reward in rewards.values())
-    assert terminals.keys() == environ.action_spaces.keys()
+    assert terminals.keys() == environ.action_spaces.spaces.keys()
     assert all(isinstance(terminal, bool) for terminal in terminals.values())
-    assert infos.keys() == environ.action_spaces.keys()
+    assert infos.keys() == environ.action_spaces.spaces.keys()
     assert all(isinstance(info, dict) for info in infos.values())
 
 @pytest.mark.parametrize("environment_class", MULTI_AGENT_ENVIRONMENTS)
 def test_reset(environment_class):
     environ = environment_class()
     states = environ.reset()
-    assert states.keys() == environ.action_spaces.keys()
+    assert environ.observation_spaces.contains(states)
