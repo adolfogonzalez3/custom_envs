8d9f72d763875b1c943f7c188590078a0e1c8f62
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-08-27 11:16:08 -0500

    Updated env tests

--- tests/envs/test_env.py
+++ tests/envs/test_env.py
@@ -9,12 +9,15 @@ def test_step(environment_class):
     '''Tests environment's step method.'''
     environ = environment_class()
     environ.reset()
+    assert environ.current_step == 0
     action = environ.action_space.sample()
-    state, reward, terminal, info = environ.step(action)
-    assert environ.observation_space.contains(state)
-    assert isinstance(reward, float)
-    assert isinstance(terminal, bool)
-    assert isinstance(info, dict)
+    for i in range(1, 10):
+        state, reward, terminal, info = environ.step(action)
+        assert environ.current_step == i
+        assert environ.observation_space.contains(state)
+        assert isinstance(reward, float)
+        assert isinstance(terminal, bool)
+        assert isinstance(info, dict)
 
 
 @pytest.mark.parametrize("environment_class", SINGLE_AGENT_ENVIRONMENTS)
